d068949968e8deb7423c4da7114b73e7fa4cde48
Kusnick <kusnick@imada.sdu.dk>
2025-03-11 21:28:06 +0100

    Add better responsiveness

--- src/app/ProfileHeader.tsx
+++ src/app/ProfileHeader.tsx
@@ -1,18 +1,22 @@
 "use client";
 import * as React from "react";
 import { EnvelopeIcon, HomeIcon, PhoneIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
 
 export const ProfileHeader: React.FC = () => {
   return (
     <header className="grid grid-cols-[min-content_auto] grid-rows-1 items-center px-6 py-2.5 w-full bg-white max-md:px-5 sticky top-0 border-b-1 border-gray-200 z-10">
-      <figure className="hover:animate-pulse flex overflow-hidden relative z-0 flex-col items-start self-stretch my-auto min-h-[100px] rounded-[99px] w-[100px]">
-        <div className="flex absolute right-0 bottom-0 z-0 bg-neutral-200 h-[100px] min-h-[100px] w-[100px]" />
-        <img
-          src="image001-1.jpg"
+      <div className="relative flex size-18 lg:size-28">
+        <Image
+          src="/image001-1.jpg"
+          fill={true}
           alt="Jakob Kusnick profile"
-          className="object-contain absolute right-0 top-[-17px] z-0 max-w-full aspect-square h-[133px] w-[100px]"
+          className="rounded-full object-cover"
         />
-      </figure>
+      </div>
+      {/*       <figure className="hover:animate-pulse flex overflow-hidden relative z-0 flex-col items-start self-stretch my-auto min-h-[100px] rounded-[99px] w-[100px]">
+        <div className="flex absolute right-0 bottom-0 z-0 bg-neutral-200 h-[100px] min-h-[100px] w-[100px]" />
+      </figure> */}
 
       <div className="grid grid-cols-1 grid-rows-[auto_min-content] w-fit justify-self-center">
         <div className="z-0 pb-3.5 my-auto text-5xl text-neutral-700 justify-center flex">
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="text-[3vw] md:text-[2vw]">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
